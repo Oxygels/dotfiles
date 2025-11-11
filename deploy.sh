d217e0f0ff01c99cb089474e3498d2da843cc64a
Oxygels <39919597+Oxygels@users.noreply.github.com>
2025-11-11 16:13:22 +0100

    nvim: add lazygit config

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ "$#" -eq 0 ]; then
-    rsync -r -v --exclude ".git" --exclude "deploy.sh" --exclude ".placeholder" "." "$HOME"
+  rsync -r -v --exclude ".git" --exclude "deploy.sh" --exclude ".placeholder" "." ".gitmodules" "$HOME"
 
 elif [ "$#" -eq 1 ]; then
-    rsync -r -v --exclude ".git" --exclude "deploy.sh" --exclude ".placeholder" "." "$1:"
+  rsync -r -v --exclude ".git" --exclude "deploy.sh" --exclude ".placeholder" "." ".gitmodules" "$1:"
 
 else
-    echo "Usage: deploy.sh [user@]hostname"
-    exit 1
+  echo "Usage: deploy.sh [user@]hostname"
+  exit 1
 fi
