c18b5a11e48e1cfb3aefbe12840fdf700f6203a9
Oxygels <39919597+Oxygels@users.noreply.github.com>
2026-01-14 10:56:37 +0100

    fish: adapt tmux check and aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,22 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    if not set -q TMUX
+    # This matching is necessary because the $TMUX variable isn't set when opening
+    # a ssh connection through tmux
+    # However the $TERM variable is kept
+    if not string match -q 'tmux*' "$TERM"
         tmux
     end
 end
 
 export EDITOR=vim
 
-alias l="eza -l"
-alias ll="eza -la"
+if type -q eza
+    alias l="eza -l"
+    alias ll="eza -la"
+else
+    alias l="ls -l"
+    alias ll="ls -al"
+end
+
 alias ip="ip -c"
 alias lg="lazygit"
 alias c="bat"
