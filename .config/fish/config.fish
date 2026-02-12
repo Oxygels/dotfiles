e70a148bdf3228a5584d0e1274158921be2f738f
Oxygels <39919597+Oxygels@users.noreply.github.com>
2026-02-12 10:39:20 +0100

    fish: do not follow symlinks by default for fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,7 @@ if status is-interactive
 end
 
 export EDITOR=vim
-export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
+export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --exclude .git'
 
 if type -q eza
     alias l="eza -l"
