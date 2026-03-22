33b55a67e6057366777369299cfb5d45d6694c03
Oxygels <39919597+Oxygels@users.noreply.github.com>
2026-03-22 23:38:27 +0100

    nvim: better options documentation

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,20 +2,24 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Vim general settings
+local opt = vim.opt
+
 vim.filetype.add({
     filename = {
         ["kitty.conf"] = "kitty",
     },
 })
 
-local opt = vim.opt
-
+-- Indent settings
 opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 opt.cindent = true
 
+-- Visual settings
 opt.wrap = true
 
+-- LSP settings
 vim.g.lazyvim_python_lsp = "basedpyright"
